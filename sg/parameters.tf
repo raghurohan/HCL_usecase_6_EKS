951489ebd3da2d2edd6cecfb8631e28dc31c25c0
Raghu H <raghu.h@hcltech.com>
2025-09-12 15:49:11 +0530

    ddd

--- sg/parameters.tf
+++ sg/parameters.tf
@@ -4,6 +4,7 @@ resource "aws_ssm_parameter" "web_alb_sg_id" {
   name  = "/${var.project}/${var.environment}/web_alb_sg_id"
   type  = "String"
   value = module.web_alb_sg.id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "eks_control_plane_sg_id" {
@@ -11,6 +12,7 @@ resource "aws_ssm_parameter" "eks_control_plane_sg_id" {
   name  = "/${var.project}/${var.environment}/eks_control_plane_sg_id"
   type  = "String"
   value = module.eks_control_plane_sg.id
+  overwrite = true
 }
 
 
@@ -19,6 +21,7 @@ resource "aws_ssm_parameter" "node_sg_id" {
   name  = "/${var.project}/${var.environment}/node_sg_id"
   type  = "String"
   value = module.node_sg.id
+  overwrite = true
 }
 
 
