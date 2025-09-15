ed74e6c231ee48069201673b99ca4044096787ee
Raghu H <raghu.h@hcltech.com>
2025-09-15 14:06:35 +0530

    configuring target group binding

--- alb/paramters.tf
+++ alb/paramters.tf
@@ -9,3 +9,9 @@ resource "aws_ssm_parameter" "web_alb_listener_http" {
 #  type  = "String"
 #  value = aws_lb_listener.web_https.arn
 #}
+
+resource "aws-ssm_parameter" "alb_target_group_arn" {
+  name = "/${var.project}/${var.environment}/alb-target-group-arn"
+  type = "String"
+  value = aws_lb_target_group.frontend.arn
+}
