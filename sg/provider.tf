b435b1f7e6d20658360fd5c303f2a0d38ea55bbe
Raghu_H <raghuatharva95@gmail.com>
2025-09-28 19:32:20 +0530

    Update provider.tf

--- sg/provider.tf
+++ sg/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" { #backend info should always be in terraform block
     bucket         = "remotestate11"
-    key            = "expense_k8_sg_module/terraform.tfstate"
+    key            = "expense_sg_module/terraform.tfstate"
     region         = "us-east-1" # region of bucket and dynamodb
     dynamodb_table = "raghu"     #partition key should always be LockID --> case sensitive
   }
